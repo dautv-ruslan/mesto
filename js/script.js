2c9335c1172446497f7a5eb73d459cf84306515d
dautv-ruslan@rambler.ru <dautv-ruslan@rambler.ru>
2023-02-26 14:00:00 +0500

    do the job

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 /* Используемые ноды */
 
 const form = document.querySelector(".form");
+const formUser = document.querySelector('.form-user-new');
+
 const fPopup = document.querySelector('.popup');
 const fPopupContainer = document.querySelector('.popup__container');
 const closeIcon = document.querySelector('.popup__close-icon');
+const overlay = document.querySelectorAll('.popup');
 const imageCloseIcon = document.querySelector('.popup__image-close-icon');
 const cardCloseIcon = document.querySelector('.popup__card-close-icon');
 const editButton = document.querySelector('.user__edit-button');
@@ -75,11 +78,79 @@ function closePopup(item) {
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-
+    
     userName.textContent = itemName.value;
     userJobName.textContent = jobName.value;
 
     closePopup(fPopup);
+}
+
+const showSubmitButton = (submitButton) => {
+    submitButton.classList.add(submitButton.classList[0] + '_state_enabled');
+    submitButton.disabled = false;
+}
+
+const hideSubmitButton = (submitButton) => {
+    submitButton.classList.forEach(function(item) {
+        if (item.indexOf('_state_enabled') > 0) {
+            submitButton.classList.remove(item);
+        }
+        submitButton.disabled = true;
+    })
+}
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+    inputElement.classList.add('form__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('form__input-error_active');
+}
+
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+    inputElement.classList.remove('form__input_type_error');
+    errorElement.classList.remove('form__input-error_active');
+    errorElement.textContent = '';
+}
+
+const isValid = (formElement, inputElement) => {
+    const submitButtonElement = formElement.querySelector('input[type="submit"]');
+
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+        hideSubmitButton(submitButtonElement);
+    }
+    else {
+        hideInputError(formElement, inputElement);
+        if (checkEventListeners(formElement)) {
+            showSubmitButton(submitButtonElement);
+        }
+    }
+}
+
+const checkEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.form__input:not(input[type=submit])'));
+
+    let input_valid = true;
+    inputList.forEach((inputElement) => {
+        if (!inputElement.validity.valid) {
+            input_valid = false;
+        }
+    });
+
+    return input_valid;
+}
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.form__input:not(input[type=submit])'));
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement);
+        });
+    });
 }
 
 function handleFormClose(evt) {
@@ -141,9 +212,13 @@ function handleCardAdd(evt) {
     closePopup(cardAddTemplate);
 }
 
+const formInput = form.querySelector('.form__input');
+
 /* Привязка слотов и сигналов, подписчиков */
 
 form.addEventListener('submit', handleFormSubmit);
+setEventListeners(form);
+setEventListeners(formUser);
 closeIcon.addEventListener('click', handleFormClose);
 imageCloseIcon.addEventListener('click', handleImageClose);
 cardCloseIcon.addEventListener('click', handleCardClose);
@@ -168,5 +243,21 @@ document.addEventListener('DOMContentLoaded', function() {
         const cardItem = insertCard(item.name, item.link);
 
         cardList.append(cardItem);
+    });
+    overlay.forEach(function(item) {
+        item.addEventListener('click', function(evt) {
+            if (evt.target == fPopup) {
+                closePopup(fPopup)
+            }
+            if (evt.target == cardAddTemplate) {
+                closePopup(cardAddTemplate);
+            }
+        })
+    });
+    document.addEventListener('keydown', function(evt) {
+        if (evt.code == 'Escape') {
+            closePopup(fPopup);
+            closePopup(cardAddTemplate);
+        }
     });
 })
